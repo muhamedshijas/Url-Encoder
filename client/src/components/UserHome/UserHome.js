10e47a5f661118d87c4e170b7e1af35ad08031cb
muhamedshijas <shijushijas157@gmail.com>
2023-07-04 07:22:58 +0530

    user homepage updated

--- client/src/components/UserHome/UserHome.js
+++ client/src/components/UserHome/UserHome.js
@@ -10,7 +10,9 @@ import {
   MDBRow,
   MDBCol,
   MDBIcon,
-  MDBInput
+  MDBInput,
+  MDBCardTitle,
+  MDBCardText
 }
   from 'mdb-react-ui-kit';
 
@@ -35,32 +37,44 @@ function UserHome() {
     })
   }
   return (
-    <div>UserHome
-      <button onClick={handleLogout}>Logout</button>
+    <div>
+    
       <MDBContainer className="my-5">
 
         <MDBCard>
           <MDBRow className='g-0 login-section'>
 
-            <MDBCol md='6' className='login-form'>
-              <MDBCardBody className='d-flex flex-column home-body'>
-
-                <div className='d-flex flex-row mt-2'>
-                  <MDBIcon fas icon="cubes fa-3x me-3" style={{ color: '#ff6219' }} />
-                  <span className="h1 fw-bold mb-0">Url Shortner</span>
-                </div>
-                <h5 className="fw-normal my-4 pb-3" style={{ letterSpacing: '1px' }}>Sign into your account</h5>
-                <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg" />
-                <MDBBtn className="mb-4 px-5" color='dark' size='lg'  >Submit</MDBBtn>
+            <MDBCol md='6' className='url-form'>
+              <MDBCardBody className='d-flex flex-column url-home-body'>
+              <div className="logout" onClick={handleLogout}>
+              <MDBIcon fas icon="sign-out-alt" />Logout 
+              </div>
+              <div className="url-submit-form">
+              <div className='d-flex flex-row mt-2'>
+              <MDBIcon fas icon="cubes fa-3x me-3" style={{ color: '#ff6219' }} />
+              <span className="h1 fw-bold mb-0">Url Shortner</span>
+              </div>
+              <h5 className="fw-normal my-4 pb-3" style={{ letterSpacing: '1px' }}>Paste Your Link here</h5>
+              <MDBInput wrapperClass='mb-4' label='Title' id='formControlLg' type='text' size="lg" />
+              <MDBInput wrapperClass='mb-4' label='Paste your Link' id='formControlLg' type='text' size="lg" />
+              <MDBBtn className="mb-4 px-5 url-button" color='dark' size='lg'  >Submit</MDBBtn>
+              </div>
                 
               </MDBCardBody>
             </MDBCol>
 
             <MDBCol md='6' className='home-url'>
-              <MDBCard className='home-url-card'>
-                <MDBCardBody className='url-card'>This is some text within a card body.</MDBCardBody>
-              </MDBCard>
+              <MDBCard className='w-100 home-url-card'>
+            <MDBCardBody className='url-card' >
+              <MDBCardTitle>Card title</MDBCardTitle>
+              <MDBCardText>With supporting text below as a natural lead-in to additional content.</MDBCardText>
+              <MDBBtn href='#'>Button</MDBBtn>
+            </MDBCardBody>
+          </MDBCard>
             </MDBCol>
+
+
+            
 
           </MDBRow>
         </MDBCard>
