e1336827898f0884194edcace75f26548e2ed080
muhamedshijas <shijushijas157@gmail.com>
2023-07-05 12:41:06 +0530

    mongo url updated

--- Server/Config/dbConnect.js
+++ Server/Config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 function dbConnect(){
-    mongoose.connect("mongodb://127.0.0.1/urlShorter").then(result=>{
+    mongoose.connect("mongodb+srv://shijushijas157:wildHunter2.0@cluster0.k5a2dtd.mongodb.net/?retryWrites=true&w=majority").then(result=>{
         console.log("Database connected")
     }).catch((err)=>{
         console.log("data base error \n"+err)
