8750fdd327e1252d5ff7904adfb02dda5252cf4b
muhamedshijas <shijushijas157@gmail.com>
2023-07-05 12:49:21 +0530

    url updated

--- Server/Config/dbConnect.js
+++ Server/Config/dbConnect.js
@@ -3,7 +3,7 @@ function dbConnect(){
     mongoose.connect("mongodb+srv://shijushijas157:wildHunter2.0@cluster0.k5a2dtd.mongodb.net/?retryWrites=true&w=majority").then(result=>{
         console.log("Database connected")
     }).catch((err)=>{
-        console.log("data base error \n"+err)
+        console.log("database error \n"+err)
     })
 }
 export default dbConnect
