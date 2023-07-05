1c61ccaec521035afefaf0efe9dbb11a67181fd6
muhamedshijas <shijushijas157@gmail.com>
2023-07-05 15:24:47 +0530

    loading updated

--- client/src/Pages/UserRedirect.jsx
+++ client/src/Pages/UserRedirect.jsx
@@ -1,18 +1,24 @@
 import axios from 'axios'
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import ReactLoading from 'react-loading';
 
 export default function UserRedirect() {
+  const [isLoading,setIsLoading]=useState(false)
     const {id}=useParams()
     useEffect(() => {
         (async function(){
+          setIsLoading(true)
             let {data} = await axios.get("user/geturl/"+id)
+            setIsLoading(false)
             window.location.href=data.url.shortUrl
         })()
     }, [])
   return (
     <div>
-      
+      {
+        isLoading?<div className="admin-loading"> <ReactLoading type="puff" color="black" height={80} width={80} /></div>:""
+      }
     </div>
   )
 }
